8960227ff99ce6c2428646e5259864c281376949
Saurabh Kumar <asksaurabh101@gmail.com>
2023-06-01 14:53:56 +0530

    Open browser url

--- initial-project/test/specs/example.e2e.js
+++ initial-project/test/specs/example.e2e.js
@@ -1,17 +1,6 @@
-describe('My Login application', () => {
-  it('should login with valid credentials', async () => {
-    await browser.url(`https://the-internet.herokuapp.com/login`);
-
-    await $('#username').setValue('tomsmith');
-    await $('#password').setValue('SuperSecretPassword!');
-    await $('button[type="submit"]').click();
-
-    // Wait for 1 second
-    await browser.pause(1000);
-    await expect($('#flash')).toBeExisting();
-    await expect($('#flash')).toHaveTextContaining(
-      'You logged into a secure area!'
-    );
-    await browser.pause(1000);
+describe('My first test suite', () => {
+  it('My first test case', async () => {
+    let myUrl = 'https://www.example.com';
+    await browser.url(myUrl);
   });
 });
