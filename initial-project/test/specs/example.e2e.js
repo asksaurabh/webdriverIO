5a97e5a3e7a3ac2ff823e7ee8dd4d2f16e218f61
Saurabh Kumar <asksaurabh101@gmail.com>
2023-06-01 15:27:50 +0530

    Assert title and url of page

--- initial-project/test/specs/example.e2e.js
+++ initial-project/test/specs/example.e2e.js
@@ -1,5 +1,5 @@
 describe('My first test suite', () => {
-  it('My first test case', async () => {
+  it.skip('My first test case', async () => {
     let myUrl = 'https://www.example.com';
 
     await browser.url(myUrl);
@@ -7,5 +7,21 @@ describe('My first test suite', () => {
 
     await browser.url('https://www.google.com');
     await browser.pause(2000);
+  });
+
+  it('should assert browser title and url', async () => {
+    await browser.url('https://coderadio.freecodecamp.org/');
+    await browser.pause(2000);
+
+    // OLD WAY
+    const pageTitle = await browser.getTitle();
+    const pageUrl = await browser.getUrl();
+
+    expect(pageTitle).toContain('freeCodeCamp.org Code Radio');
+    expect(pageUrl).toContain('coderadio.freecodecamp.org');
+
+    // NEW WAY
+    expect(browser).toHaveTitleContaining('freeCodeCamp.org Code Radio');
+    expect(browser).toHaveUrlContaining('coderadio.freecodecamp.org');
   });
 });
