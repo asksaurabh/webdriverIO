812b68c521ff6cef53a9a501dbd27e354b3bf333
Saurabh Kumar <asksaurabh101@gmail.com>
2023-06-01 15:00:41 +0530

    Add browser pause and open different url

--- initial-project/test/specs/example.e2e.js
+++ initial-project/test/specs/example.e2e.js
@@ -1,6 +1,11 @@
 describe('My first test suite', () => {
   it('My first test case', async () => {
     let myUrl = 'https://www.example.com';
+
     await browser.url(myUrl);
+    await browser.pause(2000);
+
+    await browser.url('https://www.google.com');
+    await browser.pause(2000);
   });
 });
