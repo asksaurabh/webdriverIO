ffd44aaa4a7378fafb90e7dda3e2d8fa4fdda75c
Saurabh Kumar <asksaurabh101@gmail.com>
2023-06-03 15:57:35 +0530

    Add custom file upload command

--- initial-project/test/specs/advanced.e2e.js
+++ initial-project/test/specs/advanced.e2e.js
@@ -1,32 +1,23 @@
 describe('Advanced Testing', () => {
-  // beforeEach(async () => {
-  //   await browser.url('https://the-internet.herokuapp.com/upload');
-  // });
-
   beforeEach(async () => {
-    await loadWebsite();
+    await browser.url('https://the-internet.herokuapp.com/upload');
   });
 
   it('should test file upload 1', async () => {
     const filePath = './my-body.png';
-    const remoteFilePath = await browser.uploadFile(filePath);
-
-    await $('#file-upload').setValue(remoteFilePath);
-    await $('#file-submit').click();
+    await browser.customFileUpload(filePath);
     await browser.pause(2000);
   });
 
   it('should test file upload 2', async () => {
     const filePath = './whole-screenshot.png';
-    const remoteFilePath = await browser.uploadFile(filePath);
+    await browser.customFileUpload(filePath);
+    await browser.pause(2000);
+  });
 
-    await $('#file-upload').setValue(remoteFilePath);
-    await $('#file-submit').click();
-
+  it('should test file upload 3', async () => {
+    const filePath = './my-div.png';
+    await browser.customFileUpload(filePath);
     await browser.pause(2000);
   });
 });
-
-async function loadWebsite() {
-  await browser.url('https://the-internet.herokuapp.com/upload');
-}
