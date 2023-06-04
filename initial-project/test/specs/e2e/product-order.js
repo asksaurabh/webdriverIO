81acdb163f70780ae080415690f01ec43b99aeea
Saurabh Kumar <asksaurabh101@gmail.com>
2023-06-04 11:01:52 +0530

    Add product order test

--- initial-project/test/specs/e2e/product-order.js
+++ initial-project/test/specs/e2e/product-order.js
@@ -9,6 +9,27 @@ describe('Order Products', () => {
   });
 
   it('should complete product order', async () => {
-    await browser.pause(2000);
+    await $('#inventory_container').waitForDisplayed();
+    await $('button[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await $('button[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+    await $('.shopping_cart_link').click();
+
+    await $('.cart_list').waitForDisplayed();
+    await $('button*=Checkout').click();
+
+    await $('.checkout_info_wrapper').$('form').waitForDisplayed();
+    await $('input[data-test="firstName"]').setValue('test');
+    await $('input[data-test="lastName"]').setValue('test');
+    await $('input[data-test="postalCode"]').setValue('110001');
+    await $('input[data-test="continue"]').click();
+
+    await $('.cart_list').waitForDisplayed();
+    await $('.summary_info').waitForDisplayed();
+    await $('button*=Finish').click();
+
+    await $('#checkout_complete_container').waitForDisplayed();
+    await $('button*=Back').waitForDisplayed();
+    const completeOrderHeader = await $('h2*=Thank');
+    await expect(completeOrderHeader).toHaveText('Thank you for your order!');
   });
 });
