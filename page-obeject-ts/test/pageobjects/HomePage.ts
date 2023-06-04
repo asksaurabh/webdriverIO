ee05779b4698c5a869c0827b32acfa5093f64f0f
Saurabh Kumar <asksaurabh101@gmail.com>
2023-06-04 13:31:36 +0530

    Add methods and selectors

--- page-obeject-ts/test/pageobjects/HomePage.ts
+++ page-obeject-ts/test/pageobjects/HomePage.ts
@@ -1,6 +1,14 @@
 class HomePage {
+  public get signInButton() {
+    return $('#signin_button');
+  }
+
   public async visit() {
     await browser.url('http://zero.webappsecurity.com/');
+  }
+
+  public async clickOnSignIn() {
+    await this.signInButton.click();
   }
 }
 
--- page-obeject-ts/test/specs/example.test.ts
+++ page-obeject-ts/test/specs/example.test.ts
@@ -3,6 +3,7 @@ import HomePage from '../pageobjects/HomePage';
 describe('First test using page objects pattern', () => {
   it('should load homepage', async () => {
     await HomePage.visit();
+    await HomePage.clickOnSignIn();
     await browser.pause(2000);
   });
 });
