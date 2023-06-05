1bb08803c1f789837e64e2b55bb185ae93fba460
Saurabh Kumar <asksaurabh101@gmail.com>
2023-06-05 11:03:50 +0530

    Create data helpers for random data creation

--- page-obeject-ts/test/pageobjects/AbstractPage.ts
+++ page-obeject-ts/test/pageobjects/AbstractPage.ts
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 export default class AbstractPage {
   public async waitForSeconds(seconds: number) {
     await browser.pause(seconds * 1000);
@@ -13,5 +14,13 @@ export default class AbstractPage {
 
   public async takeScreenshot(saveToPath) {
     await browser.saveScreenshot(saveToPath);
+  }
+
+  public async generateRandomNumber() {
+    return Math.floor(Math.random() * 10000 + 1);
+  }
+
+  public async generateRandomString() {
+    return crypto.randomBytes(20).toString('hex');
   }
 }
