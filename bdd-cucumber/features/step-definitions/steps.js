4972579deec9fecf9526bc6e9131cdf2a7c9a6a6
Saurabh Kumar <asksaurabh101@gmail.com>
2023-06-05 14:36:29 +0530

    Using defineStep

--- bdd-cucumber/features/step-definitions/steps.js
+++ bdd-cucumber/features/step-definitions/steps.js
@@ -1,18 +1,18 @@
-const { Given, When, Then } = require('@wdio/cucumber-framework');
+const { defineStep } = require('@wdio/cucumber-framework');
 const LoginPage = require('../pageobjects/LoginPage');
 
-Given(/^I am on the login page$/, async () => {
+defineStep(/^I am on the login page$/, async () => {
   await LoginPage.visit();
 });
 
-When(/^I login with (\w+) and (.+)$/, async (username, password) => {
+defineStep(/^I login with (\w+) and (.+)$/, async (username, password) => {
   await LoginPage.login(username, password);
 });
 
-When(/^I login with invalid credentials$/, async () => {
+defineStep(/^I login with invalid credentials$/, async () => {
   await LoginPage.login('test', 'test');
 });
 
-Then(/^I should see an error message$/, async () => {
+defineStep(/^I should see an error message$/, async () => {
   await LoginPage.assertErrorMessage();
 });
