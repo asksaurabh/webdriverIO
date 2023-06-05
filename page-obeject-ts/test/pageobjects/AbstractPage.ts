b14bc2bd846fb73cb1787a63e2b6921eeadccfcf
Saurabh Kumar <asksaurabh101@gmail.com>
2023-06-05 10:58:23 +0530

    Add helper utility functions

--- page-obeject-ts/test/pageobjects/AbstractPage.ts
+++ page-obeject-ts/test/pageobjects/AbstractPage.ts
@@ -2,4 +2,16 @@ export default class AbstractPage {
   public async waitForSeconds(seconds: number) {
     await browser.pause(seconds * 1000);
   }
+
+  public async setFullHDResolution() {
+    await browser.setWindowSize(1980, 1080);
+  }
+
+  public async setNetworkSpeedTo3G() {
+    await browser.throttle('Regular3G');
+  }
+
+  public async takeScreenshot(saveToPath) {
+    await browser.saveScreenshot(saveToPath);
+  }
 }
